39774d5ef087b14ea871b46f4aa0ee6e8f8491de
newbie198 <990159805@qq.com>
2017-06-13 14:24:11 +0800

    shujuku

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -14,7 +14,7 @@ DATABASES = {
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'bk@321',                        # 你的数据库password
-        'HOST': '10.1.129.38',                   		   # 数据库HOST
+        'HOST': '10.1.129.36',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
